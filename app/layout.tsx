02dbc981193baa232ac0b5351d43a740e03966e5
Atharv Tare <atharv.tare.03@gmail.com>
2025-08-19 16:31:09 +0530

    chore!: main page changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,59 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Atharv Tare',
-  description: 'Atharv Tare\'s personal website',
-  generator: 'v1.0',
-}
+  title: "Atharv Tare",
+  description: "Atharv Tare's personal website",
+  generator: "v1.0",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <head>
+        <link
+          rel="shortcut icon"
+          href="/atharv028/favicon.ico"
+        />
+        <link
+          rel="icon"
+          href="/atharv028/favicon.ico"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/atharv028/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/atharv028/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/atharv028/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/atharv028/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/atharv028/android-chrome-512x512.png"
+        />
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
@@ -27,5 +64,5 @@ html {
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
