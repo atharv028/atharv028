9cd2e92d5f9f4b89d66c79ffd91150097d129acc
Atharv Tare <atharv.tare.03@gmail.com>
2025-08-20 00:10:08 +0530

    chore!: added GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,7 +61,21 @@ html {
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
+        
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E0Q34XEYRN"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-E0Q34XEYRN');
+            `,
+          }}
+        />
       </head>
+      
       <body>{children}</body>
     </html>
   );
